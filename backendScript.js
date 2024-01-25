8dd8fdb741ee9cb7734299cb0c7e0f9d7cc5a618
Ross <rossycharlton@googlemail.com>
2024-01-25 20:36:17 +0000

    Redone the function to fix a bug

--- backendScript.js
+++ backendScript.js
@@ -1,13 +1,7 @@
 var apiKey = "gudcJ94GAQUNf64z83rMQvIC5bneUXGo"
-function searchNews(searchQuery) {
+async function searchNews(searchQuery) {
     var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&api-key=${apiKey}`;
-    fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-        return data
-    });
-    return data
+    let response = await fetch(queryURL);
+    let data = await response.json();
+    return data;
  }
